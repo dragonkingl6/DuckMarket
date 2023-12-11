45514b5eac780ee4074a891d9161e5aa48a7d047
Mai Tiến Đức -Ph20223 <124181138+dragonkingl6@users.noreply.github.com>
2023-12-11 11:48:27 +0700

    Login with email and pasword

--- app/src/main/java/duc/thanhhoa/duckmarket/LoginActivity.java
+++ app/src/main/java/duc/thanhhoa/duckmarket/LoginActivity.java
@@ -1,23 +1,34 @@
 package duc.thanhhoa.duckmarket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button signIn;
     EditText name, email,pasword;
     TextView signUp;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth= FirebaseAuth.getInstance();
 
         signIn=findViewById(R.id.login_btn);
         email=findViewById(R.id.email_reg);
@@ -35,7 +46,41 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                loginUser();
             }
         });
+    }
+
+    private void loginUser() {
+        String userEmail = email.getText().toString().trim();
+        String userPassword = pasword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(userEmail)) {
+            email.setError("Email is Required");
+            return;
+        }
+        if (TextUtils.isEmpty(userPassword)) {
+            pasword.setError("Password is Required");
+            return;
+        }
+        if (userPassword.length() < 6) {
+            pasword.setError("Password must be >= 6 Characters");
+            Toast.makeText(this, "Password must be >= 6 Characters", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //login user
+
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"Logged in Successfully",Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(LoginActivity.this,"Error !"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
