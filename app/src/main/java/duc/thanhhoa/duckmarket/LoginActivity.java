4a1935aa5e39609d114bb1e2dccad422e434881f
Mai Tiến Đức -Ph20223 <124181138+dragonkingl6@users.noreply.github.com>
2023-12-11 10:40:25 +0700

    Screens Login Register

--- app/src/main/java/duc/thanhhoa/duckmarket/LoginActivity.java
+++ app/src/main/java/duc/thanhhoa/duckmarket/LoginActivity.java
@@ -2,13 +2,40 @@ package duc.thanhhoa.duckmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button signIn;
+    EditText name, email,pasword;
+    TextView signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signIn=findViewById(R.id.login_btn);
+        email=findViewById(R.id.email_reg);
+        pasword=findViewById(R.id.password_reg);
+        signUp=findViewById(R.id.sign_up);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
--- app/src/main/java/duc/thanhhoa/duckmarket/RegisterActivity.java
+++ app/src/main/java/duc/thanhhoa/duckmarket/RegisterActivity.java
@@ -2,13 +2,43 @@ package duc.thanhhoa.duckmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button signUp;
+    EditText name, email,pasword;
+    TextView signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        signUp=findViewById(R.id.login_btn);
+        name=findViewById(R.id.name_reg);
+        email=findViewById(R.id.email_reg);
+        pasword=findViewById(R.id.password_reg);
+        signIn=findViewById(R.id.sign_in);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+
+            }
+        });
+
     }
 }
